75b98396bfe2bf28503e7c72f7793d1cd0ddc435
(philnoug) <philippe.nougaillon@gmail.com>
2026-05-31 09:23:16 +0200

    Update contact_mailer.rb
    Lucas added

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -3,7 +3,7 @@
 class ContactMailer < ApplicationMailer
   def submitted(message)
     @message = message
-    mail(to: "sebastien.pourchaire@aikku.eu, philippe.nougaillon@aikku.eu, pierre-emmanuel.dacquet@aikku.eu",
+    mail(to: "sebastien.pourchaire@aikku.eu, lucas.blanquet@aikku.eu, philippe.nougaillon@aikku.eu, pierre-emmanuel.dacquet@aikku.eu",
       subject: "[WWW.AIKKU.EU] Nouveau contact")
   end
 end
